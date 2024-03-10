09db6e91cee65248599f440a3e299cef84ba92c2
Thar Linn Htet <linnhtet6102005@gmail.com>
2024-03-10 21:02:30 +0630

    two leet code added

---
+++ sortColors.ts
@@ -0,0 +1,25 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): number[] {
+  let start = 0;
+  let mid = 0;
+  let end = nums.length - 1;
+
+  while (mid <= end) {
+    if (nums[mid] === 0) {
+      [nums[start], nums[mid]] = [nums[mid], nums[start]];
+      start++;
+      mid++;
+    } else if (nums[mid] === 2) {
+      [nums[mid], nums[end]] = [nums[end], nums[mid]];
+      end--;
+    } else {
+      mid++;
+    }
+  }
+  return nums;
+}
+
+console.log(sortColors([2, 0, 2, 1, 1, 0])); // expected [0,0,1,1,2,2]
+console.log(sortColors([2, 1, 1, 0])); // expected [0,1,1,2]
---
+++ sprialMatrix.ts
@@ -0,0 +1,44 @@
+const spiralOrder = (matrix: number[][]): number[] => {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  let rowStart = 0;
+  let rowEnd = rows - 1;
+  let colStart = 0;
+  let colEnd = cols - 1;
+  const result: number[] = [];
+
+  while (rowStart <= rowEnd && colStart <= colEnd) {
+    for (let col = colStart; col <= colEnd; col++) {
+      result.push(matrix[rowStart][col]);
+    }
+    rowStart++;
+
+    for (let row = rowStart; row <= rowEnd; row++) {
+      result.push(matrix[row][colEnd]);
+    }
+    colEnd--;
+
+    if (rowStart <= rowEnd) {
+      for (let col = colEnd; col >= colStart; col--) {
+        result.push(matrix[rowEnd][col]);
+      }
+      rowEnd--;
+    }
+
+    if (colStart <= colEnd) {
+      for (let row = rowEnd; row >= rowStart; row--) {
+        result.push(matrix[row][colStart]);
+      }
+      colStart++;
+    }
+  }
+
+  return result;
+};
+
+const result = spiralOrder([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
+console.log(result);
